e4cc3132754f240821282b3df0a6decac8ecbb1e
pawel <pawi_xu@interia.pl>
2017-11-25 18:40:29 +0100

    First initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Press any key to continue...";
+    getchar();
+    return 0;
+}
